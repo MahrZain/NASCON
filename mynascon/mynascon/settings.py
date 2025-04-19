a14dd5fc23db8b363dd009e922807667c9c0a575
Mehar Zain <zainzainpubg1122@gmail.com>
2025-04-19 15:04:09 +0500

    refactor: update settings for production and enhance URL patterns with names

--- mynascon/mynascon/settings.py
+++ mynascon/mynascon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y1kjgfo1+$4v1^^5@#!9^$n%5buqghwfyftrgnbq=qn&1w8elc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
